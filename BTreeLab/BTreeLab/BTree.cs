d3404279f9cb4fd5aaf437f8e3527695b2c5dd32
supershowwei <supershowwei@gmail.com>
2022-09-30 07:42:48 +0800

    first commit

---
+++ BTreeLab/BTreeLab/BTree.cs
@@ -0,0 +1,103 @@
+﻿namespace BTreeLab;
+
+public class BTree
+{
+    private readonly int maxKeyLength;
+    private BTreeNode root;
+
+    public BTree(int maxKeyLength)
+    {
+        this.maxKeyLength = maxKeyLength;
+    }
+
+    public void Add(int key)
+    {
+        BTreeNode node;
+
+        if (this.root == null)
+        {
+            this.root = new BTreeNode(this.maxKeyLength);
+
+            node = this.root;
+        }
+        else
+        {
+            node = FindLeaf(this.root, key);
+        }
+
+        node.Add(key);
+
+        this.Count++;
+    }
+
+    private static BTreeNode FindLeaf(BTreeNode node, int key)
+    {
+        if (node.IsLeaf) return node;
+
+        var index = 0;
+
+        for (; index < node.KeyCount; index++)
+        {
+            if (key.CompareTo(node.Keys[index]) < 0)
+            {
+                return FindLeaf(node.Children[index], key);
+            }
+        }
+
+        return FindLeaf(node.Children[index], key);
+    }
+
+    public int Count { get; private set; }
+}
+
+public class BTreeNode
+{
+    private readonly int maxKeyLength;
+
+    public BTreeNode(int maxKeyLength)
+    {
+        this.maxKeyLength = maxKeyLength;
+        this.Keys = new int?[maxKeyLength];
+        this.Children = new BTreeNode[maxKeyLength + 1];
+    }
+
+    public int?[] Keys { get; }
+
+    public BTreeNode[] Children { get; }
+
+    public int KeyCount { get; private set; }
+
+    public bool IsLeaf => this.Children[0] == null;
+
+    public void Add(int key)
+    {
+        if (this.KeyCount < this.maxKeyLength)
+        {
+            var index = 0;
+
+            for (; index < this.KeyCount; index++)
+            {
+                var comparison = key.CompareTo(this.Keys[index]);
+
+                if (comparison == 0) return;
+                if (comparison < 0) break;
+            }
+
+            for (var i = index; i < this.KeyCount; i++)
+            {
+                (this.Keys[index], this.Keys[i + 1]) = (this.Keys[i + 1], this.Keys[index]);
+            }
+
+            this.Keys[index] = key;
+            this.KeyCount++;
+        }
+        else
+        {
+            var median = this.KeyCount / 2 + 1;
+
+
+        }
+
+        throw new NotImplementedException();
+    }
+}
---
+++ BTreeLab/BTreeLab/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BTreeLab;
+
+var btree = new BTreeG<int>(3);
+
+var keys = Enumerable.Range(0, 10).Select(x => Random.Shared.Next(1, 3));
+
+foreach (var key in keys)
+{
+    btree.Insert(key);
+}
+
+Console.WriteLine("Hello, World!");
